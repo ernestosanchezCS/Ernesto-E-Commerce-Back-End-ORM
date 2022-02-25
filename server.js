568f95ea5f05fa541d23e84556fd0388c09f6633
Ernesto Sanchez <ernestosanchez8888@gmail.com>
2022-02-25 01:32:09 -0500

    Working on models progress made

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //allows us to pass json objects in html body
+app.use(express.urlencoded({ extended: true })); //allows us to pass query information within url
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+    console.log(`App listening on port ${PORT}!`);
 });
